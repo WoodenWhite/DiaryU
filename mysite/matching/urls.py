85ae8e5719fdcf27581dc52fb49372c32d3f4af9
zerychao <zzy_0104@126.com>
2018-04-28 16:08:30 +0800

    Add get_openId and get_openId_action to /matching/url.py

--- mysite/matching/urls.py
+++ mysite/matching/urls.py
@@ -18,4 +18,6 @@ urlpatterns = [
          name='get_user_diary_action'),
     path('alt_diary/', views.alt_diary, name='alt_diary'),
     path('alt_diary_action/', views.alt_diary_action, name='alt_diary_action'),
+    path('get_openId/', views.get_openId, name='get_openId'),
+    path('get_openId_action/', views.get_openId_action, name='get_openId_action')
 ]
